18b4d5283928bc39eb02e7331566b6d696c7ce2c
Joshua Sullivan <jsullivan@microsoft.com>
2025-11-13 17:35:43 -0600

    Initial Commit

---
+++ TwentyTwenty/TwentyTwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyTwentyApp.swift
+//  TwentyTwenty
+//
+//  Created by Joshua Sullivan on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TwentyTwentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
